20c1d5cbe442f9e88d234b0e7b056a21e64e0192
devHudi <devhudi@gmail.com>
2022-01-28 01:58:02 +0900

    feat: add ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
+
+import themes from 'assets/themes/themes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themes.light}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
