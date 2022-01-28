ac78cff1c3b5db006190c9758053037d48388437
devHudi <devhudi@gmail.com>
2022-01-28 16:43:23 +0900

    feat: return data instead of response object

--- src/apis/scheduleBlock.ts
+++ src/apis/scheduleBlock.ts
@@ -3,15 +3,15 @@ import { ScheduleBlockDto } from './dtos';
 
 export const getScheduleBlocksByTimetableId = async (timetableId: string) => {
   const res = await axios.get<ScheduleBlockDto.Response>(`/timetables/${timetableId}/scheduleblocks`);
-  return res;
+  return res.data;
 };
 
 export const createTimetable = async (createScheduleBlockRequestDto: ScheduleBlockDto.CreateRequest) => {
   const res = await axios.post<ScheduleBlockDto.Response[]>('/scheduleblock', createScheduleBlockRequestDto);
-  return res;
+  return res.data;
 };
 
 export const updateTimetable = async (updateScheduleBlockRequestDto: ScheduleBlockDto.UpdateRequest) => {
   const res = await axios.put<ScheduleBlockDto.Response[]>('/scheduleblock', updateScheduleBlockRequestDto);
-  return res;
+  return res.data;
 };
--- src/apis/timetable.ts
+++ src/apis/timetable.ts
@@ -3,15 +3,15 @@ import { TimetableDto } from './dtos';
 
 export const getTimetable = async (timetableId: string) => {
   const res = await axios.get<TimetableDto.Response>(`/timetables/${timetableId}`);
-  return res;
+  return res.data;
 };
 
 export const createTimetable = async (createTimetableRequestDto: TimetableDto.CreateRequest) => {
   const res = await axios.post<TimetableDto.Response>('/timetable', createTimetableRequestDto);
-  return res;
+  return res.data;
 };
 
 export const updateTimetable = async (updateTimetableRequestDto: TimetableDto.UpdateRequest) => {
   const res = await axios.put<TimetableDto.Response>('/timetable', updateTimetableRequestDto);
-  return res;
+  return res.data;
 };
