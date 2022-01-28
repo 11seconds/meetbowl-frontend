4e3c53bd7a25c742838b2eb1049c86d5bd56c852
devHudi <devhudi@gmail.com>
2022-01-28 19:45:33 +0900

    feat: implement CircleButton component

---
+++ src/components/common/CircleButton.tsx
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components';
+import { MdOutlineIosShare } from 'react-icons/md';
+
+type ColorType = 'red'; // 현재는 단일 색상
+
+type CircleButtonWrapper = {
+  color: ColorType;
+};
+
+type CircleButton = {
+  color: ColorType;
+  icon: 'share'; // 현재는 단일 아이콘
+};
+
+const CircleButtonWrapper = styled.button<CircleButtonWrapper>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 20px;
+  font-size: 20px;
+  cursor: pointer;
+
+  ${(props) =>
+    props.color === 'red' &&
+    css`
+      background-color: ${props.theme.colors.generic.secondaryRed};
+    `}
+
+  ${(props) =>
+    props.color === 'red' &&
+    css`
+      color: ${props.theme.colors.generic.primaryRed};
+    `}
+`;
+
+const CircleButton = ({ color, icon }: CircleButton) => (
+  <CircleButtonWrapper color={color}>{icon === 'share' && <MdOutlineIosShare />}</CircleButtonWrapper>
+);
+
+export default CircleButton;
