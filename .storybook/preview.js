5d6268c3186bd37dd32fa64e947080f402a1330d
devHudi <devhudi@gmail.com>
2022-01-28 17:29:24 +0900

    feat: add decorator into storybook preview for theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import themes from 'assets/themes';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={themes.light}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
