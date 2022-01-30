a83b5776d2af261f1c2bfe6354fcd9c1a3f902a4
devHudi <devhudi@gmail.com>
2022-01-30 12:03:50 +0900

    feat: add axios interceptors

--- src/utils/customAxios.ts
+++ src/utils/customAxios.ts
@@ -1,7 +1,33 @@
 import axios, { AxiosInstance } from 'axios';
+import { getAccessToken } from './token';
 
 const customAxios: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_HOST,
 });
+
+// 요청 인터셉터
+customAxios.interceptors.request.use(
+  (config) => {
+    const customHeaders = {
+      Authorization: `bearer ${getAccessToken()}`,
+    };
+
+    const customConfig = { ...config, headers: { ...config.headers, ...customHeaders } };
+    return customConfig;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+customAxios.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default customAxios;
