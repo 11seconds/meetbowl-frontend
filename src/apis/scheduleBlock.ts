935087c5e6fd2b35c6ef167b842804822b269018
devHudi <devhudi@gmail.com>
2022-01-30 14:24:02 +0900

    feat: clarify return type of api functions

--- src/apis/scheduleBlock.ts
+++ src/apis/scheduleBlock.ts
@@ -1,17 +1,21 @@
 import axios from 'utils/customAxios';
 import { ScheduleBlockDto } from './dtos';
 
-export const getScheduleBlocksByTimetableId = async (timetableId: string) => {
+export const getScheduleBlocksByTimetableId = async (timetableId: string): Promise<ScheduleBlockDto.Response> => {
   const res = await axios.get<ScheduleBlockDto.Response>(`/timetables/${timetableId}/scheduleblocks`);
   return res.data;
 };
 
-export const createTimetable = async (createScheduleBlockRequestDto: ScheduleBlockDto.CreateRequest) => {
+export const createTimetable = async (
+  createScheduleBlockRequestDto: ScheduleBlockDto.CreateRequest
+): Promise<ScheduleBlockDto.Response[]> => {
   const res = await axios.post<ScheduleBlockDto.Response[]>('/scheduleblock', createScheduleBlockRequestDto);
   return res.data;
 };
 
-export const updateTimetable = async (updateScheduleBlockRequestDto: ScheduleBlockDto.UpdateRequest) => {
+export const updateTimetable = async (
+  updateScheduleBlockRequestDto: ScheduleBlockDto.UpdateRequest
+): Promise<ScheduleBlockDto.Response[]> => {
   const res = await axios.put<ScheduleBlockDto.Response[]>('/scheduleblock', updateScheduleBlockRequestDto);
   return res.data;
 };
