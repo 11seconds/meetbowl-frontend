9b23a2f297fcb211babe6cae34854b0e4f39502b
devHudi <devhudi@gmail.com>
2022-01-30 20:03:39 +0900

    feat: implement BottomSheet component

---
+++ src/components/timeTable/BottomSheet.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 32px;
+  height: 50px;
+`;
+
+const Body = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 32px;
+  height: 135px;
+  border-top: 1px solid ${(props) => props.theme.colors.specific.timetableLine};
+`;
+
+type BottomSheetProps = {
+  header: JSX.Element | string;
+  body?: JSX.Element | string;
+};
+
+const BottomSheet = ({ header, body }: BottomSheetProps) => (
+  <Wrapper>
+    <Header>{header}</Header>
+    {body && <Body>{body}</Body>}
+  </Wrapper>
+);
+
+BottomSheet.defaultProps = {
+  body: null,
+};
+
+export default BottomSheet;
---
+++ src/stories/timeTable/BottomSheet.stories.tsx
@@ -0,0 +1,32 @@
+import BottomSheet from 'components/timeTable/BottomSheet';
+import NameGrid from 'components/timeTable/NameGrid';
+import SubmitterName from 'components/timeTable/SubmitterName';
+import Typography from 'components/common/Typography';
+
+export default {
+  title: 'timeTable/BottomSheet',
+  component: BottomSheet,
+};
+
+export const Default = () => <BottomSheet header="This is header" body="And... This is body" />;
+
+export const OnlyHeader = () => <BottomSheet header="I have no body" />;
+
+export const WithNames = () => (
+  <BottomSheet
+    header={<Typography weight="bold">9명 제출</Typography>}
+    body={
+      <NameGrid>
+        <SubmitterName color="#FF97D5" name="USER NAME 1" />
+        <SubmitterName color="#28D0F5" name="USER NAME 2" />
+        <SubmitterName color="#FA8B25" name="USER NAME 3" />
+        <SubmitterName color="#A32FFF" name="USER NAME 4" />
+        <SubmitterName color="#3F46F7" name="USER NAME 5" />
+        <SubmitterName color="#FFCC18" name="USER NAME 6" />
+        <SubmitterName color="#F23737" name="USER NAME 7" />
+        <SubmitterName color="#232323" name="USER NAME 8" />
+        <SubmitterName color="#1ABC2A" name="USER NAME 9" />
+      </NameGrid>
+    }
+  />
+);
