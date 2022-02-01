af70bd3f6f68a70875dd64befebfd20dc9035819
devHudi <devhudi@gmail.com>
2022-02-01 21:03:18 +0900

    feat: change style of BottomSheet

--- src/components/timeTable/BottomSheet.tsx
+++ src/components/timeTable/BottomSheet.tsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  position: fixed;
-  bottom: 0;
   width: 100%;
   background-color: #ffffff;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
