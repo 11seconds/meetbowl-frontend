585acd8323ae7ebb4e10cb88739ef89f6a6d6f88
devHudi <devhudi@gmail.com>
2022-02-04 03:24:21 +0900

    feat: bind create timetable api

--- src/pages/CreateTimetable.tsx
+++ src/pages/CreateTimetable.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useMutation } from 'react-query';
 
 import useCurrentUser from 'hooks/useCurrentUser';
+import { timetable } from 'apis';
 
 import MobileScreen from 'components/common/MobileScreen';
 import FullScreenButtonTemplate from 'components/templates/FullScreenButtonTemplate';
@@ -13,18 +15,32 @@ const CreateTimetable = () => {
   const { timetableId } = useParams();
   const { isLoading, isActive } = useCurrentUser();
 
+  const [title, setTitle] = useState('');
+
   useEffect(() => {
     if (!isLoading && !isActive) {
       navigate(`/sign-up`);
     }
   }, [navigate, isLoading, isActive, timetableId]);
 
+  const mutation = useMutation(async () => {
+    const { id } = await timetable.createTimetable({ title, description: '' });
+    navigate(`/timetable/${id}`);
+  });
+
+  const disabled = useMemo(() => {
+    if (title.length === 0) return true;
+    return false;
+  }, [title]);
+
   return (
     <MobileScreen>
       <FullScreenButtonTemplate
-        content={<TextField align="center" placeholder="시간표 제목을 입력하세요" />}
+        content={
+          <TextField align="center" placeholder="시간표 제목을 입력하세요" onChange={(e) => setTitle(e.target.value)} />
+        }
         button={
-          <Button color="red" size="lg">
+          <Button color="red" size="lg" disabled={disabled} onClick={() => mutation.mutate()}>
             생성하기
           </Button>
         }
