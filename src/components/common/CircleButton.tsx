3680f320193576111b3e356caacfc12b16f1a86d
Kim Dongwoo <andandend2847@gmail.com>
2022-02-17 00:38:39 +0900

    feat: Implement share button to copy timetable link
    시간표 공유용 링크 복사 버튼 개발

--- src/components/common/CircleButton.tsx
+++ src/components/common/CircleButton.tsx
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import { MdOutlineIosShare } from 'react-icons/md';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 type ColorType = 'red'; // 현재는 단일 색상
 
@@ -10,6 +11,7 @@ type CircleButtonWrapperProps = {
 type CircleButtonProps = {
   color: ColorType;
   icon: 'share'; // 현재는 단일 아이콘
+  url?: string; // 공유버튼으로 활용시
 };
 
 const CircleButtonWrapper = styled.button<CircleButtonWrapperProps>`
@@ -36,8 +38,14 @@ const CircleButtonWrapper = styled.button<CircleButtonWrapperProps>`
     `}
 `;
 
-const CircleButton = ({ color, icon }: CircleButtonProps) => (
-  <CircleButtonWrapper color={color}>{icon === 'share' && <MdOutlineIosShare />}</CircleButtonWrapper>
+const CircleButton = ({ color, icon, url }: CircleButtonProps) => (
+  <CopyToClipboard text={url || ''}>
+    <CircleButtonWrapper color={color}>{icon === 'share' && <MdOutlineIosShare />}</CircleButtonWrapper>
+  </CopyToClipboard>
 );
+
+CircleButton.defaultProps = {
+  url: '',
+};
 
 export default CircleButton;
